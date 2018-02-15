8fbf2707d3e6262897bf0c9cf3272e75a23427da
Emile Nijssen <emile@athom.nl>
2018-02-15 17:48:58 +0100

    migrate to yargs instead of commander.js

--- config.js
+++ config.js
@@ -4,4 +4,6 @@ module.exports = {
 	athomApiClientId: '598d85a330e1bb0c0d75b8eb',
 	athomApiClientSecret: 'ba93fc861b204732607169fb29c2708f1da7e17f',
 	athomApiLoginUrl: 'https://go.athom.com/cli-login',
+	athomMessageUrl: 'https://go.athom.com/cli-message',
+	athomMessageUrl: 'https://etc.athom.com/athom-cli.json', // TODO
 }
--- index.js
+++ index.js
@@ -3,7 +3,8 @@
 const pkg = require('./package.json');
 
 module.exports.Log = console.log;
-module.exports.Settings = new (require('./lib/Settings'));
-module.exports.AthomApi = new (require('./lib/AthomApi'));
+module.exports.Settings = new (require('./lib/Settings'))();
+module.exports.AthomApi = new (require('./lib/AthomApi'))();
+module.exports.AthomMessage = new (require('./lib/AthomMessage'))();
 module.exports.App = require('./lib/App');
 module.exports.Animation = require('./lib/Animation');

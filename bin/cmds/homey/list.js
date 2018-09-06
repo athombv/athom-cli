7fa5743417b0cc3d5532b11a730b1a3c608cc928
Emile Nijssen <emile@athom.nl>
2018-09-06 14:31:29 +0200

    add apiVersion

--- bin/cmds/homey/list.js
+++ bin/cmds/homey/list.js
@@ -7,15 +7,16 @@ const colors = require('colors');
 
 exports.desc = 'List all Homeys';
 exports.handler = async () => {
-	
+
 	try {
 		let homeys = await AthomApi.getHomeys();
-		
+
 		let table = new Table({
 			head: [
 				'ID',
 				'Name',
 				'Version',
+				'API',
 				'Language',
 				'State',
 				'Users',
@@ -23,16 +24,17 @@ exports.handler = async () => {
 				'USB',
 			].map( title => colors.white.bold(title) ),
 		});
-		
+
 		homeys.sort(( a, b ) => {
 			return -(a.state || '').localeCompare( (b.state || '') );
 		})
-		
-		homeys.forEach(homey => {						
+
+		homeys.forEach(homey => {
 			table.push([
 				homey._id,
 				homey.name,
 				homey.softwareVersion,
+				homey.apiVersion,
 				homey.language,
 				AthomApi.getFormattedState( homey ),
 				homey.users && homey.users.length,
@@ -40,7 +42,7 @@ exports.handler = async () => {
 				homey.usb ? 'Yes' : '-',
 			].map( value => value || '-' ))
 		})
-		
+
 		Log(table.toString());
 	} catch( err ) {
 		Log(err);
